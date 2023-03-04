072aaeedaad0a473c673bdef17205b6a548ba878
mkm02 <mkm02@byu.edu>
2023-03-04 12:31:41 -0700

    Add button class, game constructor, helper functions, and added to button classes in html

--- play.js
+++ play.js
@@ -4,3 +4,63 @@ const btnDescriptions = [
     {file: 'sound3.mp3', hue: 60},
     {file: 'sound4.mp3', hue: 240},
 ];
+
+class Button {
+    constructor(description, element) {
+        this.element = element;
+        this.hue = description.hue;
+        this.sound = loadSound(description.file);
+        this.paint(25);
+    }
+
+    paint(level) {
+        const background = `hsl(${this.hue}, 100%, ${level}%)`;
+        this.element.style.backgroundColor = background;
+    }
+
+    async press(volume) {
+        this.paint(50);
+        await this.play(volume);
+        this.paint(25);
+    }
+
+    async play(volume = 1.0) {
+        this.sound.volume = volume;
+        await new Promise((resolve) => {
+            this.sound.onended = resolve;
+            this.sound.play();
+        });
+    }
+}
+
+class Game {
+    buttons;
+    allowPlayer;
+    sequence;
+    playerPlaybackPos;
+    mistakeSound;
+
+    constructor() {
+        this.buttons = new Map();
+        this.allowPlayer = false;
+        this.sequence = [];
+        this.playerPlaybackPos = 0;
+        this.mistakeSound = loadSound('error.mp3');
+
+        document.querySelectorAll('.game-button').forEach((element, i) => {
+            if (i < btnDescriptions.length) {
+                this.buttons.set(element.id, new Button(btnDescriptions[i], element));
+            }
+        });
+
+        const playerNameEl = document.querySelector('.player-name');
+        playerNameEl.textContent = this.getPlayerName();
+    }
+
+}
+
+const game = new Game();
+
+function loadSound(filename) {
+    return new Audio('assets/' + filename);
+  }
