ea1a83c6b25618f34bbfa19d2111007d29e5bcb3
mkm02 <mkm02@byu.edu>
2023-03-26 20:19:04 -0600

    updated for login functionality and ui

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+
+const authCookieName = 'token';
 
 // The service port. In production the application is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
+
+app.use(cookieParser());
 
 // Serve up the applications static content
 app.use(express.static('public'));
@@ -15,23 +21,80 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({msg: 'Existing user'});
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id});
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized'});
+});
+
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token});
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown'});
+});
+
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
 // GetScores
-apiRouter.get('/scores', async (_req, res) => {
+secureApiRouter.get('/scores', async (_req, res) => {
   const scores = await DB.getHighScores();
   res.send(scores);
 });
 
 // SubmitScore
-apiRouter.post('/score', async (req, res) => {
+secureApiRouter.post('/score', async (req, res) => {
   DB.addScore(req.body);
   const scores = await DB.getHighScores();
   res.send(scores);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
 });
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', {root: 'public'});
 });
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
