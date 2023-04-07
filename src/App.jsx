8b447a1e181ada4f68512c3f1ddd3e2ca6c41340
mkm02 <mkm02@byu.edu>
2023-04-06 20:59:50 -0600

    Update to React, keeping old files temporarily

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+import { AuthState } from './login/authState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+
+  // Asynchronously determine if the user is authenticated by calling the service
+  const [authState, setAuthState] = React.useState(AuthState.Unknown);
+  React.useEffect(() => {
+    if (userName) {
+      fetch(`/api/user/${userName}`)
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json();
+          }
+        })
+        .then((user) => {
+          const state = user?.authenticated ? AuthState.Authenticated : AuthState.Unauthenticated;
+          setAuthState(state);
+        });
+    } else {
+      setAuthState(AuthState.Unauthenticated);
+    }
+  }, [userName]);
+
+  return (
+    <div className='body bg-dark text-light'>
+      <header className='container-fluid'>
+        <nav className='navbar fixed-top navbar-dark'>
+          <div className='navbar-brand'>
+            Simon<sup>&reg;</sup>
+          </div>
+          <menu className='navbar-nav'>
+            <li className='nav-item'>
+              <NavLink className='nav-link' to=''>
+                Login
+              </NavLink>
+            </li>
+            {authState === AuthState.Authenticated && (
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='play'>
+                  Play
+                </NavLink>
+              </li>
+            )}
+            {authState === AuthState.Authenticated && (
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='scores'>
+                  Scores
+                </NavLink>
+              </li>
+            )}
+            <li className='nav-item'>
+              <NavLink className='nav-link' to='about'>
+                About
+              </NavLink>
+            </li>
+          </menu>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Login
+              userName={userName}
+              authState={authState}
+              onAuthChange={(userName, authState) => {
+                setAuthState(authState);
+                setUserName(userName);
+              }}
+            />
+          }
+          exact
+        />
+        <Route path='/play' element={<Play userName={userName} />} />
+        <Route path='/scores' element={<Scores />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+
+      <footer className='bg-dark text-dark text-muted'>
+        <div class="container-fluid">
+          <span class="text-reset">Mason Miller</span>
+          <a class="text-reset"
+            href="https://github.com/mkm02/simon">
+            GitHub
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+  
+}
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
+export default App;
